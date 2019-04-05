4c8c4fe732b9eb9461c54209f08b1bb8031b5a36
Rafael Guerra <rafaelsguerra@hotmail.com>
2019-04-05 01:24:07 -0300

    versao 1 com 55 noticias

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import re
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -18,14 +19,33 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
-        #
-        #
-        #
+        section = response.url.split("/")[-2]
+        
+        for href in response.css("h3.entry-title.td-module-title a::attr(href)").getall():
+            
+            #request url must be string
+            request = scrapy.Request(href, callback = self.parse_href)
+            request.meta['section'] = section
+            yield request
+
+        #for href in response.css("h3.entry-title.td-module-title a::attr(href)"):
+        #    yield response.follow(href, callback = self.parse_href)
+        
+    def parse_href(self, response):
+        for element in response.css('article'):
+            yield {
+                'title' : element.css("div.td-post-header.td-pb-padding-side header h1::text").get(),
+                'subtitle' : "",
+                'author' : element.css("div.td-post-header.td-pb-padding-side header div.td-post-author-name a::text").get(),
+                'date' : self.format_date(element.css("time::attr(datetime)").get()),
+                'section' : response.meta['section'],
+                'text' : " ".join(element.css("div.td-post-content.td-pb-padding-side p::text").getall()[:-1]),
+                'url' : response.url,
+            }
+
+    def format_date(self, string):
+        splitted_date = re.split("[-T:+]+", string)
+        splitted_date[0], splitted_date[2] = splitted_date[2], splitted_date[0]
+        string_date = " ".join(splitted_date[:3]).replace(" ", "/") + " " + " ".join(splitted_date[3:6]).replace(" ", ":")
+        
+        return string_date
